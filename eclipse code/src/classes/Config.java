8381086401fb63d9ed6d17412906c843f2a09d49
Емельян Богданов <luciusartiuscastus68@gmail.com>
2026-04-30 11:15:16 +0100

    can save settings at .properties , but got some errors

--- eclipse code/src/classes/Config.java
+++ eclipse code/src/classes/Config.java
@@ -5,6 +5,13 @@ import java.io.FileNotFoundException;
 
 
 
+// start dependencies for properties file
+import java.util.Properties;
+import java.io.InputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+
 
 public class Config {
 	public static String 
@@ -18,7 +25,8 @@ public class Config {
 	RESTORE_PATH = "/TrashBin/to_restore",
 	REMINDER_PATH = "/Reminder",
 	PATH_OF_IDS = "/Ids",
-	DATABASE_PATH = "/tasks/database";
+	DATABASE_PATH = "/tasks/database",
+	SETTINGS_FILE = "/settings.properties";
 
 	// functions
 	public static String getRootPath(){
@@ -26,6 +34,9 @@ public class Config {
 	}
 	public static String getSettingsPath(){
 		return Config.getRootPath() + Config.SETTINGS_PATH;
+	}
+	public static String getSettingsFilePath(){
+		return getSettingsPath() + Config.SETTINGS_FILE;
 	}
 	public static String getRestorePath(){
 		return Config.getRootPath() + Config.RESTORE_PATH;
@@ -49,6 +60,10 @@ public class Config {
 	public static String getDatabasePath(){
 		return getRootPath() + Config.DATABASE_PATH;
 	}
+	public static String getLogsPath(){
+		return getRootPath() + Config.LOGS_PATH;
+	}
+
 
 	// get user paths
 	public static String getUserHomePath(){
@@ -69,11 +84,6 @@ public class Config {
 		return Config.getUserHomePath() + "/Desktop";
 	}
 
-	///////// main function 
-	public static void main(String[] args){
-		Config.createWorkspaceProjectStructure();
-	}
-
 	// create files if not exists + save settings
 	public static void createWorkspaceFolder(){
 		createFolder(Config.getRootPath());
@@ -83,18 +93,20 @@ public class Config {
 		// define the map of all 'paths'
 		String[] map = new String[]{
 			getRootPath(),
-			getSettingsPath(),
-			getRestorePath(),
-			getXmlPath(),
-			getJsonPath(),
-			getDeletePath(),
-			getIdsPath(),
-			getReminderPath(),
-			getDatabasePath()
+			getSettingsPath(), // settings
+			getRestorePath(), // restore
+			getXmlPath(), // xml
+			getJsonPath(), // json
+			getDeletePath(), // deleted
+			getIdsPath(), // ids
+			getReminderPath(), // reminders
+			getDatabasePath(), // database
+			getLogsPath(), // wich logs are stored in
 		};
 		for(String folderPath : map){
 			Config.createFolder(folderPath);
 		}
+		saveSettingsAtFile();
 	}
 	public static void createFolder(String path){
 		try {
@@ -103,5 +115,37 @@ public class Config {
 		}catch(Exception e){
 			System.out.printf("[ ERROR ] createFolder() can't create Folder in path : %s \n" , path);
 		}
+	}
+	public static void saveSettingsAtFile(){
+		Properties prop = new Properties();
+		try {
+   			
+   			InputStream propertiesInputStream =  Config.class.getClassLoader().getResourceAsStream("/Constants.properties");
+            prop.load(propertiesInputStream);
+			// set variables
+			prop.setProperty("SETTINGS_PATH",getSettingsPath());
+			prop.setProperty("ALLOWED_EXT",ALLOWED_EXT[0] + "," + ALLOWED_EXT[1]);
+			prop.setProperty("RESTORE_PATH",getRootPath());
+			prop.setProperty("DELETED_PATH",getDeletePath());
+			prop.setProperty("XML_TASK_PATH",getXmlPath());
+			prop.setProperty("LOGS_PATH",getLogsPath());
+			prop.setProperty("REMINDER_PATH",getReminderPath());
+			prop.setProperty("DATABASE_PATH",getDatabasePath());
+			prop.setProperty("SETTINGS_FILE",getSettingsPath());
+
+			// update file
+			prop.store(new FileOutputStream(getSettingsFilePath()), null);
+			System.out.printf("Settings saved at %s",getSettingsFilePath());
+
+		}
+		catch(Exception a){
+			System.out.println("Can't create settings.properties file ");
+			a.printStackTrace();
+		}
+
+	}
+		///////// main function 
+	public static void main(String[] args){
+		Config.createWorkspaceProjectStructure();
 	}
 }
