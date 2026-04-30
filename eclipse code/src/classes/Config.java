77d1c9cbd4775d40aefd16ac471b09695e071b5a
Емельян Богданов <luciusartiuscastus68@gmail.com>
2026-04-30 10:32:22 +0100

    Config.java can init the project structure folder
    now Config.java can : 
    - create all folders of the project
    - get their Paths

--- eclipse code/src/classes/Config.java
+++ eclipse code/src/classes/Config.java
@@ -3,9 +3,12 @@ import java.io.File;
 import java.io.FileNotFoundException;
 
 
+
+
+
 public class Config {
 	public static String 
-	ROOT_PATH = Config.getRootPath(),
+	ROOT_PATH = "/workspace",
 	SETTINGS_PATH = "/Settings",
 	ALLOWED_EXT[] = {"json","xml"},
 	JSON_TASKS_PATH = "/tasks/json",
@@ -13,11 +16,13 @@ public class Config {
 	LOGS_PATH = "/logs",
 	DELETED_PATH = "/TrashBin/deleted",
 	RESTORE_PATH = "/TrashBin/to_restore",
-	PATH_OF_IDS = "/Ids";
+	REMINDER_PATH = "/Reminder",
+	PATH_OF_IDS = "/Ids",
+	DATABASE_PATH = "/tasks/database";
 
 	// functions
 	public static String getRootPath(){
-		return getStartUpFolder();
+		return getStartUpFolder() + ROOT_PATH;
 	}
 	public static String getSettingsPath(){
 		return Config.getRootPath() + Config.SETTINGS_PATH;
@@ -25,17 +30,24 @@ public class Config {
 	public static String getRestorePath(){
 		return Config.getRootPath() + Config.RESTORE_PATH;
 	}
+	public static String getDeletePath(){
+		return Config.getRootPath() + Config.DELETED_PATH;
+	}
+
 	public static String getXmlPath(){
 		return Config.getRootPath() + Config.XML_TASK_PATH;
 	}
 	public static String getJsonPath(){
 		return Config.getRootPath() + Config.JSON_TASKS_PATH;
 	}
-	public static String getDeletePath(){
-		return Config.getRootPath() + Config.DELETED_PATH;
+	public static String getIdsPath(){
+		return Config.getRootPath() + Config.PATH_OF_IDS;
 	}
-	public static String getStartUpFolder(){
-		return Config.getUserHomePath() + "/AppData/Roaming/Microsoft/Windows/Start Menu/Programs";
+	public static String getReminderPath(){
+		return Config.getRootPath() + Config.REMINDER_PATH;
+	}
+	public static String getDatabasePath(){
+		return getRootPath() + Config.DATABASE_PATH;
 	}
 
 	// get user paths
@@ -49,10 +61,47 @@ public class Config {
 			return null;
 		}
 	}
+
+	public static String getStartUpFolder(){
+		return Config.getUserHomePath() + "/AppData/Roaming/Microsoft/Windows/Start Menu/Programs";
+	}
 	public static String getDesktopPath(){
 		return Config.getUserHomePath() + "/Desktop";
 	}
+
+	///////// main function 
 	public static void main(String[] args){
-		System.out.println(Config.getDeletePath());
+		Config.createWorkspaceProjectStructure();
+	}
+
+	// create files if not exists + save settings
+	public static void createWorkspaceFolder(){
+		createFolder(Config.getRootPath());
+		System.out.println("[ INFO ] Workspace Folder created !");
+	}
+	public static void createWorkspaceProjectStructure(){
+		// define the map of all 'paths'
+		String[] map = new String[]{
+			getRootPath(),
+			getSettingsPath(),
+			getRestorePath(),
+			getXmlPath(),
+			getJsonPath(),
+			getDeletePath(),
+			getIdsPath(),
+			getReminderPath(),
+			getDatabasePath()
+		};
+		for(String folderPath : map){
+			Config.createFolder(folderPath);
+		}
+	}
+	public static void createFolder(String path){
+		try {
+			new File(path).mkdirs();
+			System.out.printf("[ INFO ] Folder %s created successfully \n",path);
+		}catch(Exception e){
+			System.out.printf("[ ERROR ] createFolder() can't create Folder in path : %s \n" , path);
+		}
 	}
 }
