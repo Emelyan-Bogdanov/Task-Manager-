1496e5766c67b4bc6a434d420e6fc328d2a7f9a1
Емельян Богданов (idwahman) <luciusartiuscastus68@gmail.com>
2026-04-30 12:47:38 +0100

    add current time & date support to get datetime

--- eclipse code/src/classes/Task.java
+++ eclipse code/src/classes/Task.java
@@ -1,6 +1,11 @@
+
+// dependencies for Date & Time 
+import java.time.LocalDate; // for date
+import java.time.LocalTime; // for time
+
 public class Task {
 	// id unique pour chaque task
-	public static ID = 0;
+	public static int ID = 0;
 	// path a sauvegarder les tâches
 	public static String SAVE_PATH = "";
 	// 
@@ -52,7 +57,7 @@ public class Task {
 	 public void saveTask(String exportType , String filename){
 	 	// verify that the task title or the description not empty
 	 	if (this.title.equals("") || this.description.equals("")){
-	 		System.out.println("ERROR : le titre et la description vide !!!")
+	 		System.out.println("ERROR : le titre et la description vide !!!");
 	 	}
 
 	 	// save as the type
@@ -73,25 +78,6 @@ public class Task {
 
 	// from Task object to File
 	public void  saveAsJSON(String filename){
-		json.add("ID", this.id);
-			  json.add("TITLE", this.title);
-			  json.add("DESCRIPTION",this.description);
-			  json.add("SHEDULE",this.this.sheduled);
-			  json.add("CREATED_AT",this.createAt);
-			  json.add("STATUS",this.status);
-			  json.add("IMPORTANCE",this.importance);
-			  json.addLast();
-			  json.toJson();
-
-			// save to file
-		try (FileWriter file = new FileWriter(filename)) {
-			file.write(json.toJson());
-			System.out.printf("[SUCCESS] : Fichier %s sauvegardé avec succés" , filename);
-		} catch (IOException e) {
-			e.printStackTrace();
-			System.out.printf("ERROR : Impossible de sauvegarder : %s ",filename);
-		}
-
 	}
 	public void saveAsHTML(String filename){
 		// il est mieu de sauvegarder le lien vers tous les tâches dans un serveur flask et templates en html
@@ -99,52 +85,50 @@ public class Task {
 	}
 
 	/* ====================   SAVE/LOAD  TASK AS/FROM .properties ============================ */
-	public static void TaskToProperties(Task task){
+	public void ToPropertiesFile(){
 		Properties prop = new Properties();
 		try {
 
 			// set variables
-			prop.setProperty("id",task.id);
-			prop.setProperty("title",task.title);
-			prop.setProperty("text",task.text);
-			prop.setProperty("description",task.description);
-			prop.setProperty("status",task.status);
-			prop.setProperty("importance",task.importance + "");
-			prop.setProperty("createdAt",task.createdAt);
-			prop.setProperty("hasDeadLine",task.hasDeadLine?"true":"false");
-			prop.setProperty("deleted",task.DELETED?"true":"false");
-			prop.setProperty("canBeRestored",task.canBeRestored ? "true":"false");
-			prop.setProperty("taskDeadLine",task.taskDeadLine);
-			prop.setProperty("tag-count",task.tags.length + "");
+			prop.setProperty("id",this.id);
+			prop.setProperty("title",this.title);
+			prop.setProperty("text",this.text);
+			prop.setProperty("description",this.description);
+			prop.setProperty("status",this.status);
+			prop.setProperty("importance",this.importance + "");
+			prop.setProperty("createdAt",this.createdAt);
+			prop.setProperty("hasDeadLine",this.hasDeadLine?"true":"false");
+			prop.setProperty("deleted",this.DELETED?"true":"false");
+			prop.setProperty("canBeRestored",this.canBeRestored ? "true":"false");
+			prop.setProperty("taskDeadLine",this.taskDeadLine);
+			prop.setProperty("tag-count",this.tags.length + "");
 
 			// loop over tags to create property key : tag-*=Sport where * is the index of that tag
-			for (int i = 0 ; i < task.tags.length;i++){
-				prop.setProperty("tag-" + i , task.tags[i]);
+			for (int i = 0 ; i < this.tags.length;i++){
+				prop.setProperty("tag-" + i , this.tags[i]);
 			}
 			prop.setProperty("color");
 			prop.setProperty("subtask-count");
 			String[] subtasks = null ; // get all subtasks
 
 			// loop over tags to create property key : subtask-*-title or subtask-*-text , where * is the index of that tag
-			for (int i = 0 ; i < subtasks.length;i++){
-				prop.setProperty("subtask-"+ i + "-title",subtasks[i].title);
-				prop.setProperty("subtask-" + i +"-text",subtasks[i].text);
+			for (int i = 0 ; i < this.subtasks.length;i++){
+				prop.setProperty("subtask-"+ i + "-title", this.subtasks[i].title);
+				prop.setProperty("subtask-" + i +"-text", this.subtasks[i].text);
 			}
 
 			// update file
 			prop.store(new FileOutputStream(getSettingsFilePath()), null);
 			/////// !! Save this action to log
-			
-
 		}
 		catch(Exception a){
 			System.out.println("Can't create settings.properties file ");
 			a.printStackTrace();
 		}
-
 	}
 
 }
+
 /*
 	use this method to write json object to a file : 
 		- mapper.writeValue(new File("user.json"), userObject);
